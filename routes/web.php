83d341ac1bd3828b8d40f8c54cb0d43f42d819ca
unknown <lucas.oliveira019@hotmail.com>
2024-08-25 20:04:32 -0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PrincipalController;
+use App\Http\Controllers\SobreNosController;
+use App\Http\Controllers\ContatoController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [PrincipalController::class, 'principal']);
+
+Route::get('/contato', [SobreNosController::class, 'sobreNos']);
+
+Route::get('/sobre-nos', [ContatoController::class, 'contato']);
+
+
